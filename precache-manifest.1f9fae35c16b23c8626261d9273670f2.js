afffe6ca5f13fb47685d58b4100ee7307d844047
Mike <15151851516@163.com>
2021-05-14 23:46:24 +0800

    deploy

---
+++ precache-manifest.1f9fae35c16b23c8626261d9273670f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cdea1cdc91b497d33f",
+    "url": "/wu-fish-money-website/css/app.b10d374b.css"
+  },
+  {
+    "revision": "9a7f40e6e3ece6d6608f711533f7e751",
+    "url": "/wu-fish-money-website/index.html"
+  },
+  {
+    "revision": "d2cdea1cdc91b497d33f",
+    "url": "/wu-fish-money-website/js/app.c45d58bd.js"
+  },
+  {
+    "revision": "1aa2c622dadce109e436",
+    "url": "/wu-fish-money-website/js/chunk-vendors.e896cd8f.js"
+  },
+  {
+    "revision": "e30d315a0f0094e2713678abc2ca2d15",
+    "url": "/wu-fish-money-website/manifest.json"
+  },
+  {
+    "revision": "1037cdd1c96ba0239d57ad74f49744d0",
+    "url": "/wu-fish-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wu-fish-money-website/robots.txt"
+  }
+]);
